8e66777c26d5ac4717d4f6ec0a804d2b1e15fd52
Joakim Abrahamsson <joakim.abra@gmail.com>
2021-11-11 13:49:59 +0100

    Static class som innehåller metod som returnerar user ID ifall angivet user ID existerar, Dummyklass för att testa.

---
+++ LostMyLighterGroup4/LogIn/LogIn.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LostMyLighterGroup4.MenuClasses
+{
+    static class LogIn
+    {
+        static bool userFound = false;
+        static int userID;
+        
+
+        //Metod kollar inskrivet ID med sparade ID:n från skapade DummyUsers
+        //Måste uppdateras med riktiga Userklassen
+        public static int CheckUser()
+        {
+            while (true)
+            {
+                Console.Write("Skriv in ditt användar-ID: ");
+                try
+                {
+                    userID = Convert.ToInt32(Console.ReadLine());
+                }
+                catch(Exception)
+                {
+                }
+                foreach (int item in DummyUser.UsersIDS.Keys)
+                {
+                    if (userID == item)
+                    {
+                        userFound = true;
+                    }
+                }
+                    switch (userFound)
+                {
+                    case true:
+                        //PasswordCheckMethod();
+                        Console.WriteLine("Du är inloggad");
+                        return userID;
+                            
+                    default:
+                        Console.WriteLine("Ej giltigt användar-ID");
+                        break;
+                }
+            }
+        }
+    }
+}
