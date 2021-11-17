421329ab130e013c5b1451765b341760694cb981
joakim-abra <joakim.abra@gmail.com>
2021-11-17 13:45:57 +0100

    test

--- LostMyLighterGroup4/ManagerClasses/ProgramManager.cs
+++ LostMyLighterGroup4/ManagerClasses/ProgramManager.cs
@@ -9,56 +9,61 @@ namespace LostMyLighterGroup4
 {
     class ProgramManager
     {
-        
+        private int userActive;
         //Nån sorts metod för att avgjöra vilket meny som skall köras
         // User  ID=0 = standard??
-        public void Presenter(int userActive)
+        public void Presenter()
         {
-                    default:
+            switch (userActive)
+            {
+                default:
                     //LoggedinUserChoice(LoggedInUserMenuObject.UserMenu("Name"));
-                        break;
-                    case 0:
+                    break;
+                case 0:
                     //StandardUserChoice( standarMenuObject.MainMenu());
-                        break;
+                    break;
+            }
         }
         //Kör metoder beroende på val från LoggedInMenu objekt
         public void LoggedinUserChoice(int selection)
         {
-            MenuClasses.LoggedInMenu
-            switch(selection)
-            {
-                case 1:
-                    //run print active marschalls method
-                    break;
-                case 2:
-                    //run search marschalls method
-                    break;
-                case 3:
-                    //run create new marschall method
-                    break;         
-                case 4:
-                    //run add lostlighter method
-                    break;
-                case 5:
-                    // run print profile info method
-                    break;
-                case 6:
-                    // run change user name method
-                    break;
-                case 7:
-                    //run change age method
-                    break;
-                case 8:
-                    //run change user adress method
-                    break;
-                case 0:
-                    return;
-            }
+
+                switch (selection)
+                {
+                    case 1:
+                        //run print active marschalls method
+                        break;
+                    case 2:
+                        //run search marschalls method
+                        break;
+                    case 3:
+                        //run create new marschall method
+                        break;
+                    case 4:
+                        //run add lostlighter method
+                        break;
+                    case 5:
+                        // run print profile info method
+                        break;
+                    case 6:
+                        // run change user name method
+                        break;
+                    case 7:
+                        //run change age method
+                        break;
+                    case 8:
+                        //run change user adress method
+                        break;
+                    case 0:
+                        return;
+                }
+            Presenter();
         }
 
         //Kör metoder beroende på output från StandardMenu objekt
         public void StandardUserChoice(int selection)
         {
+            
             switch(selection)
             {
                 case 1:
@@ -77,15 +82,12 @@ namespace LostMyLighterGroup4
                 case 0:
                     return;
             }
-            
+            Presenter();
         }
         //Konstruktor som samtidigt skapar nytt standarmeny-objekt och kör menymetoden, vet inte ifall vi skall göra så här       
        public ProgramManager()
         {
-            MenuClasses.StandardMenu standard = new MenuClasses.StandardMenu();
-            ManagerClasses.UserManager standardManager = new ManagerClasses.UserManager();
-            new LogIn.DummyUser();
-            standard.MainMenu();
+
         }
 
     }
