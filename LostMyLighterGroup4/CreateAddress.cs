b93e75c612646499781d8506199334f933f9b1d4
mane <mane@LAPTOP-QQQDMLUG>
2021-11-18 15:13:40 +0100

    Skapat klassen och metoder.

---
+++ LostMyLighterGroup4/CreateAddress.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LostMyLighterGroup4
+{
+    public static class CreateAddress
+    {
+        // Inmatningskontroll etc kommer läggas till.
+        public static string AddStreet(string userInput)
+        {
+            Console.Write("Gatuadress: ");
+            string street = userInput;
+            return street;
+        }
+
+        public static int AddPostCode(int userInput)
+        {
+            Console.Write("Postnummer: ");
+            int pCode = userInput;
+            return pCode;
+        }
+
+        public static string AddPostTown(string userInput)  
+        {
+            Console.Write("Postort: ");
+            string pTown = userInput;
+            return pTown;
+        }
+
+    }
+}
