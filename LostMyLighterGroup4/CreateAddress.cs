c659871893c41d1b7d5ddd92ca9d8ce5b31be187
majnabbe <89972519+majnabbe@users.noreply.github.com>
2021-11-19 11:50:28 +0100

    Inmatningsmetoder påbörjade. AddPostCode klar (?). (#51)
    Co-authored-by: mane <mane@LAPTOP-QQQDMLUG>

--- LostMyLighterGroup4/CreateAddress.cs
+++ LostMyLighterGroup4/CreateAddress.cs
@@ -6,29 +6,91 @@ using System.Threading.Tasks;
 
 namespace LostMyLighterGroup4
 {
-    public static class CreateAddress
+    // Sökfunktionen måste ta hänsyn till hur adressen lagras. Kanske bättre att formattera på annat ställe?
+    // Skriva i menyn att det går att lämna postnummerfältet tomt. Går ej på de andra fälten.
+
+    public static class CreateAddress   
     {
-        // Inmatningskontroll etc kommer läggas till.
-        public static string AddStreet(string userInput)
+       public static string AddStreet() // Ej klar.
         {
             Console.Write("Gatuadress: ");
-            string street = userInput;
+
+            string street = Console.ReadLine();
+
+            street = street.ToLower();
+
             return street;
         }
 
-        public static int AddPostCode(int userInput)
+        public static string AddPostCode()
         {
-            Console.Write("Postnummer: ");
-            int pCode = userInput;
+            Console.Write("Postnummer; ");
+
+            List<char> postCodeList = new List<char>();// Lista för att kunna justera inmatningen.
+
+            bool loop = true;
+
+            while (loop)
+            {
+                postCodeList.Clear(); // Nollställ så att loopen inte lägger till element.
+
+                int j = 0;
+
+                string postCode = Console.ReadLine();
+
+                postCodeList.AddRange(postCode);
+
+                if (!string.IsNullOrWhiteSpace(postCode))// Möjliggör att postnummerfältet lämnas tomt.
+                {
+                    for (int i = postCodeList.Count - 1; i >= 0; i--)// Tar bort ev. mellanslag.
+                    {
+                        if (postCodeList[i] == ' ') 
+                        {
+                            postCodeList.Remove(postCodeList[i]);
+                        }
+                    }
+
+                    foreach (char item in postCodeList)// Kollar om något annat än tal angivits eller om listan innehåller fler siffror än tillåtet.
+                    {
+                        if (!Char.IsDigit(item) || postCodeList.Count > 5)// Kan sättas till != 5 om inga inkompletta inmatningar tillåts.
+                        {
+                            Console.Write("Felaktig inmatning. Försök igen: ");
+
+                            break;
+                        }
+
+                        if (j == postCodeList.Count - 1)// Bryter loopen vid korrekt inmatning.
+                        {
+                            loop = false;
+                        }
+
+                        j++;
+                    }
+                }
+                else// Om inget postnummer angivits.
+                {
+                    Console.WriteLine("Ingen uppgift.");
+
+                    loop = false;
+                }
+            }
+
+            string pCode = new string(postCodeList.ToArray());// Listan tillbaka till string.
+
+            pCode = pCode.Insert(3, " ");// Formattering av postnumret (XXX XX).
+
             return pCode;
         }
 
-        public static string AddPostTown(string userInput)  
+        public static string AddPostTown() // Ej klar.
         {
             Console.Write("Postort: ");
-            string pTown = userInput;
+
+            string pTown = Console.ReadLine();
+
+            pTown = pTown.ToLower();
+
             return pTown;
         }
-
     }
 }
