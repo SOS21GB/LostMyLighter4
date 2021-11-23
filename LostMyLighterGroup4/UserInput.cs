72fc2755785e9ef5497fe5c6cfb2da6c2b635709
Edwin Forss <eddan.forss@gmail.com>
2021-11-23 17:40:54 +0100

    ta in användare

---
+++ LostMyLighterGroup4/UserInput.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LostMyLighterGroup4
+{
+    class UserInput
+    {
+		public static void UserCreate()
+		{
+			Console.WriteLine("skriv ditt namn: ");
+			try
+			{
+				string _name = Console.ReadLine();
+
+
+			}
+			catch
+			{
+				Console.WriteLine("Error try again");
+			}
+
+
+			Console.WriteLine("skriv ditt Id: ");
+			try
+			{
+
+
+				int _id = Convert.ToInt32(Console.ReadLine());
+			}
+			catch
+			{
+				Console.WriteLine("Error try again");
+			}
+
+
+			Console.WriteLine("skriv din ålder: ");
+			try
+			{
+
+
+				int _age = Convert.ToInt32(Console.ReadLine());
+			}
+			catch
+			{
+				Console.WriteLine("Error try again");
+			}
+
+
+			Console.WriteLine("skriv din adress: ");
+			try
+			{
+
+
+				string _address = Console.ReadLine();
+			}
+			catch
+			{
+				Console.WriteLine("Error try again");
+			}
+
+
+
+
+
+
+		}
+
+	}
+}
