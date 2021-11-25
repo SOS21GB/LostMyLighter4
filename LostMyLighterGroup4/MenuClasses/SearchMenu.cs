fb830a4fc05ae3df2ee6cc6008c512915eebe5f6
mane <mane@LAPTOP-QQQDMLUG>
2021-11-25 13:18:53 +0100

    Lagt till/tagit bort utskrift + lite språkändringar.

--- LostMyLighterGroup4/MenuClasses/SearchMenu.cs
+++ LostMyLighterGroup4/MenuClasses/SearchMenu.cs
@@ -31,7 +31,6 @@ namespace LostMyLighterGroup4.MenuClasses
                         Marschall.PrintAllMarschalls();
                         break;
                     case "0":
-                        Console.WriteLine("Återgå till huvudmenyn.");
                         return;
                     default:
                         Console.WriteLine("Felaktig inmatning. Försök igen.");
@@ -43,11 +42,12 @@ namespace LostMyLighterGroup4.MenuClasses
         private void PrintSearchMenu()
         {
             PrintSeparator();
-            Console.WriteLine("Sök eller visa.".PadLeft(19));
-            Console.WriteLine("1) Sök på adress. ");
-            Console.WriteLine("2) Sök på postnummer.");
+            Console.WriteLine("Sök efter eller visa marschaller.".PadLeft(19));
+            Console.WriteLine("1) Sök efter marschall baserat på adress.");
+            Console.WriteLine("2) Sök efter marschall baserat på postnummer.");
             Console.WriteLine("3) Visa alla aktiva marschaller.");
             Console.WriteLine("4) Visa alla registerade marschaller.");
+            Console.WriteLine("0) Återgå till huvudmenyn.");
         }
 
         #region Hjälpmetod
