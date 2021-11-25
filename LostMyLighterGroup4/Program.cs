02b9e79573a6c2325cb4c2de1ff2594369c67a58
Edwin Forss <eddan.forss@gmail.com>
2021-11-25 21:05:32 +0100

    public class med user input

--- LostMyLighterGroup4/Program.cs
+++ LostMyLighterGroup4/Program.cs
@@ -4,19 +4,90 @@ using System.Collections.Generic;
 
 namespace LostMyLighterGroup4
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            DummyUser name = new DummyUser();
-            DummyUser name2 = new DummyUser();
-            List<int> users = new List<int>();
-            foreach(int item in DummyUser.UsersIDS.Keys)
-            {
-                users.Add(item);
-            }
-                LogIn.CheckUser(users);
+	public static class UserInput
+	{
 
-        }
-    }
+
+
+		public static void UserCreate()
+		{
+
+
+
+
+
+			int i = 0;
+
+			while (i == 0)
+			{
+				Console.WriteLine("skriv ditt namn: ");
+				try
+				{
+					string name = Console.ReadLine();
+					i++;
+
+				}
+				catch
+				{
+					Console.WriteLine("Error try again");
+				}
+			}
+
+
+
+
+
+			while (i == 1)
+			{
+				Console.WriteLine("skriv din ålder: ");
+				try
+				{
+					int age = Convert.ToInt32(Console.ReadLine());
+					i++;
+				}
+				catch
+				{
+					Console.WriteLine("Error try again");
+				}
+			}
+
+
+			while (i == 2)
+			{
+				Console.WriteLine("skriv din adress: ");
+				try
+				{
+					string address = Console.ReadLine();
+					i++;
+				}
+				catch
+				{
+					Console.WriteLine("Error try again");
+				}
+
+
+
+			}
+
+			while (i == 3)
+			{
+				Console.WriteLine("skriv antalet borttappade tändare: ");
+				try
+				{
+					int lostLighters = Convert.ToInt32(Console.ReadLine());
+					i++;
+
+				}
+				catch
+				{
+					Console.WriteLine("Error try again");
+				}
+			}
+
+
+
+
+		}
+
+	}
 }
