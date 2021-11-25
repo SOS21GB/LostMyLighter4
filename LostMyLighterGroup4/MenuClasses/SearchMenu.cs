c499ef30bb947a8fe66bed5a82e0c2693154c7bf
mane <mane@LAPTOP-QQQDMLUG>
2021-11-25 17:10:54 +0100

    Diverse förändringar.

--- LostMyLighterGroup4/MenuClasses/SearchMenu.cs
+++ LostMyLighterGroup4/MenuClasses/SearchMenu.cs
@@ -18,22 +18,38 @@ namespace LostMyLighterGroup4.MenuClasses
 
                 switch (userInput)
                 {
-                    case "1":
+                    case "1":   
+                        Console.Clear();
                         Marschall.FindStreet();
+                        Console.WriteLine("\nTryck på valfri tangent för att fortsätta.");
+                        Console.ReadKey();
                         break;
                     case "2":
+                        Console.Clear();
                         Marschall.FindPostCode();
+                        Console.WriteLine("\nTryck på valfri tangent för att fortsätta.");
+                        Console.ReadKey();
                         break;
                     case "3":
+                        Console.Clear();
                         Marschall.PrintActiveMarschalls();
-                            break;
+                        Console.WriteLine("\nTryck på valfri tangent för att fortsätta.");
+                        Console.ReadKey();
+                        break;
                     case "4":
+                        Console.Clear();
                         Marschall.PrintAllMarschalls();
+                        Console.WriteLine("\nTryck på valfri tangent för att fortsätta.");
+                        Console.ReadKey();
                         break;
                     case "0":
+                        Console.Clear();
                         return;
                     default:
+                        Console.Clear();
                         Console.WriteLine("Felaktig inmatning. Försök igen.");
+                        Console.WriteLine("\nTryck på valfri tangent för att fortsätta.");
+                        Console.ReadKey();
                         break;
                 }
             }
@@ -41,20 +57,29 @@ namespace LostMyLighterGroup4.MenuClasses
 
         private void PrintSearchMenu()
         {
-            PrintSeparator();
-            Console.WriteLine("Sök efter eller visa marschaller.".PadLeft(19));
+            PrintSeparator2();
+            Console.WriteLine("Hitta marschaller.".PadLeft(30));
+            PrintShortSeparator2();
             Console.WriteLine("1) Sök efter marschall baserat på adress.");
             Console.WriteLine("2) Sök efter marschall baserat på postnummer.");
+            PrintShortSeparator2();
             Console.WriteLine("3) Visa alla aktiva marschaller.");
             Console.WriteLine("4) Visa alla registerade marschaller.");
+            PrintShortSeparator2();
             Console.WriteLine("0) Återgå till huvudmenyn.");
+            PrintSeparator2();
+            Console.Write("Ditt val: ");
         }
 
         #region Hjälpmetod
         //Printar separatorer till menyn
-        private void PrintSeparator()
+        private void PrintShortSeparator2()
         {
-            Console.WriteLine("*--------------------------*");
+            Console.WriteLine("*----------------*".PadLeft(30));
+        }
+        private void PrintSeparator2()
+        {
+            Console.WriteLine("*--------------------------------------------*");
         }
         #endregion
 
