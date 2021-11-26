66f0424eba4b72518c3a5a20d651efed699b4ff1
Edwin Forss <eddan.forss@gmail.com>
2021-11-26 16:44:06 +0100

    Change Town

---
+++ LostMyLighterGroup4/ChangeTown.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LostMyLighterGroup4
+{
+    class ChangeTown
+    {
+        public static void ChangePostCode(int id)
+        {
+
+
+            string PostTown = "";
+            Console.WriteLine("Skriv in din nya postort: ");
+
+            PostTown = Console.ReadLine();
+
+            Console.WriteLine("Din nya gata är: {0}", PostTown);
+
+            User.GetUserById(id).pTown = PostTown;
+
+
+
+
+        }
+    }
+}
