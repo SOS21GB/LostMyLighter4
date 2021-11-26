a405504cf3e8cc63bc151f59ff1fe59b82413e8e
Edwin Forss <eddan.forss@gmail.com>
2021-11-26 12:01:38 +0100

    input för user

---
+++ LostMyLighterGroup4/Userinput1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LostMyLighterGroup4
+{
+	public static class InputUser
+	{
+
+
+
+		public static void UserCreate()
+		{
+
+			string name;
+			int age;
+			string address;
+			int lostLighters;
+
+			int i = 0;
+
+			while (i == 0)
+			{
+				Console.WriteLine("skriv ditt namn: ");
+				try
+				{
+					name = Console.ReadLine();
+					i++;
+				}
+				catch
+				{
+					Console.WriteLine("Error try again");
+				}
+			}
+
+
+
+
+
+			while (i == 1)
+			{
+				Console.WriteLine("skriv din ålder: ");
+				try
+				{
+					age = Convert.ToInt32(Console.ReadLine());
+					i++;
+
+				}
+				catch
+				{
+					Console.WriteLine("Error try again");
+				}
+			}
+
+
+			while (i == 2)
+			{
+				Console.WriteLine("skriv din adress: ");
+				try
+				{
+					address = Console.ReadLine();
+					i++;
+
+				}
+				catch
+				{
+					Console.WriteLine("Error try again");
+				}
+
+
+
+			}
+
+			while (i == 3)
+			{
+				Console.WriteLine("skriv antalet borttappade tändare: ");
+				try
+				{
+					lostLighters = Convert.ToInt32(Console.ReadLine());
+
+				}
+				catch
+				{
+					Console.WriteLine("Error try again");
+				}
+			}
+
+            new User(name, age, address, lostLighters);
+
+			User user = new User(name, age, address, lostLighters);
+			user.PrintUser();
+
+
+
+
+
+
+
+		}
+
+	}
+
+}
