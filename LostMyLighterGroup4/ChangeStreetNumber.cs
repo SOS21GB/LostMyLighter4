88f0d03fde0bc6112cdd77bfe56cbc9c3be85b41
Edwin Forss <eddan.forss@gmail.com>
2021-11-26 16:31:35 +0100

    För att ändra postcode

---
+++ LostMyLighterGroup4/ChangeStreetNumber.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LostMyLighterGroup4
+{
+    class ChangeStreetNumber
+    {
+        public static void ChangePostCode(int id)
+        {
+            int postcode = 0;
+            Console.WriteLine("Skriv in din nya postnummer utan mellanslag: ");
+            postcode = Convert.ToInt32(Console.ReadLine());
+            Convert.ToString(postcode);
+            User.GetUserById(id).pCode = postcode;
+
+            Console.WriteLine("Ditt nya postnummer är: {0}", postcode);
+
+
+
+
+        }
+    }
+}
