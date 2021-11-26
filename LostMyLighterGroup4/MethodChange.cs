5cc2dd064f329cb8f7c839de673b9a5384ee9be9
Niloofar22 <89391875+Niloofar22@users.noreply.github.com>
2021-11-26 15:59:34 +0100

    Create MethodChange.cs (#123)

---
+++ LostMyLighterGroup4/MethodChange.cs
@@ -0,0 +1,49 @@
+﻿using System;
+namespace LostMyLighterGroup4
+{
+    public class MethodChange
+    {
+        public static void ChangeUserMethod(int id)
+        {
+            int Input = 0;
+
+            do
+            {
+                Console.WriteLine(" Choose what do you want to change? ");
+                Console.WriteLine("1.  Name:");
+                Console.WriteLine("2.  Age:");
+
+
+                try
+                {
+                    Input = Convert.ToInt32(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.Clear();
+
+                }
+
+            } while (Input == 0);
+
+            switch (Input)
+            {
+                case 1:
+
+                    Console.WriteLine("Ändra namn: ");
+                    User.GetUserById(id).Name = Console.ReadLine();
+                   
+                    break;
+
+                case 2:
+                    Console.WriteLine("Ändra ålder: ");
+                    User.GetUserById(id).Age = Convert.ToInt32(Console.ReadLine());
+                    break;
+
+
+            }
+
+
+        }
+    }
+}
