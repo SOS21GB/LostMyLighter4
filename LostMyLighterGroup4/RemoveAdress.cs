ecc7fe3f279982c58a02bee6645bc4e628bbd9a5
Edwin Forss <eddan.forss@gmail.com>
2021-11-26 13:39:16 +0100

    ändra gata

---
+++ LostMyLighterGroup4/RemoveAdress.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LostMyLighterGroup4
+{
+    class ChangeStreet
+    {
+        public static void ÄndraGata()
+        {
+            string street = "";
+            Console.WriteLine("Skriv in din nya gata: ");
+
+            street = Console.ReadLine();
+
+            Console.WriteLine("Din nya gata är: { }", street);
+
+
+
+
+        }
+        
+    }
+}
